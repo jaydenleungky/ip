4f346ca7f4050293652c6d6cb1c01364189022c7
Jayden <jaydenleung2011@gmail.com>
2026-08-20 11:31:09 +0800

    Change to use task class for implementation

--- src/main/java/Coco.java
+++ src/main/java/Coco.java
@@ -18,8 +18,7 @@ public class Coco {
         System.out.println("What can I do for you?");
         System.out.println(LINE);
 
-        String[] tasks = new String[MAX_TASKS];
-        boolean[] isDone = new boolean[MAX_TASKS];
+        Task[] tasks = new Task[MAX_TASKS];
         int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
@@ -33,21 +32,20 @@ public class Coco {
             if (input.equals("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    String checkbox = isDone[i] ? "[X]" : "[ ]";
-                    System.out.println((i + 1) + "." + checkbox + " " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
             } else if (input.startsWith("mark ")) {
                 int index = Integer.parseInt(input.substring(5).trim()) - 1;
-                isDone[index] = true;
+                tasks[index].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [X] " + tasks[index]);
+                System.out.println("  " + tasks[index]);
             } else if (input.startsWith("unmark ")) {
                 int index = Integer.parseInt(input.substring(7).trim()) - 1;
-                isDone[index] = false;
+                tasks[index].markAsNotDone();
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  [ ] " + tasks[index]);
+                System.out.println("  " + tasks[index]);
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
             }
