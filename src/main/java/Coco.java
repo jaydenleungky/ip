7cc427365d608dd2bb10bfbcf2de03ef8e907f37
Jayden <jaydenleung2011@gmail.com>
2026-08-20 08:52:49 +0800

    Add greet user and exit

--- src/main/java/Coco.java
+++ src/main/java/Coco.java
@@ -1,10 +1,21 @@
 public class Coco {
+    private static final String LINE =
+            "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = "  ____ ___   ____ ___  \n"
                 + " / ___/ _ \\ / ___/ _ \\ \n"
                 + "| |  | | | | |  | | | |\n"
                 + "| |__| |_| | |__| |_| |\n"
                 + " \\____\\___/ \\____\\___/ \n";
+
+        System.out.println(LINE);
         System.out.println(banner);
+        System.out.println("Hello! I'm Coco.");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
     }
 }
